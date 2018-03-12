61af137c7d99acc1a745487b0f00ffebf9b70fd5
Judzhin Miles <info@msbios.com>
2018-03-12 13:26:32 +0200

    Lazy Composer Commiter

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\CPanel\Exception;
+
+/**
+ * Class RuntimeException
+ * @package MSBios\CPanel\Exception
+ */
+class RuntimeException extends \RuntimeException
+{
+
+}
