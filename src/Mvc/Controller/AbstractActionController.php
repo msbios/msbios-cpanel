64606d0a69dc98c9c4df6f9f8531d531702f75bd
Judzhin Miles <info@msbios.com>
2018-03-21 14:30:59 +0200

    Lazy Composer Commiter

--- src/Mvc/Controller/AbstractActionController.php
+++ src/Mvc/Controller/AbstractActionController.php
@@ -6,22 +6,39 @@
 
 namespace MSBios\CPanel\Mvc\Controller;
 
-use MSBios\Guard\GuardInterface;
 use Zend\Mvc\Controller\AbstractActionController as DefaultAbstractActionController;
-use Zend\Permissions\Acl\Resource\ResourceInterface;
 
 /**
  * Class AbstractActionController
  * @package MSBios\CPanel\Mvc\Controller
  */
 abstract class AbstractActionController extends DefaultAbstractActionController implements
-    ActionControllerInterface,
-    GuardInterface,
-    ResourceInterface
+    ActionControllerInterface
 {
 
     /** @const DEFAULT_ITEM_COUNT_PER_PAGE */
     const DEFAULT_ITEM_COUNT_PER_PAGE = 10;
+
+    /** @const EVENT_PRE_PERSIST_DATA */
+    const EVENT_PRE_PERSIST_DATA = 'pre.persist.data';
+
+    /** @const EVENT_POST_PERSIST_DATA */
+    const EVENT_POST_PERSIST_DATA = 'post.persist.data';
+
+    /** @const EVENT_PRE_MERGE_DATA */
+    const EVENT_PRE_MERGE_DATA = 'pre.merge.data';
+
+    /** @const EVENT_POST_MERGE_DATA */
+    const EVENT_POST_MERGE_DATA = 'post.merge.data';
+
+    /** @const EVENT_PRE_REMOVE_DATA */
+    const EVENT_PRE_REMOVE_DATA = 'pre.remove.data';
+
+    /** @const EVENT_POST_REMOVE_DATA */
+    const EVENT_POST_REMOVE_DATA = 'post.remove.data';
+
+    /** @const EVENT_VALIDATE_ERROR */
+    const EVENT_VALIDATE_ERROR = 'validate.error';
 
     /**
      * @return string
--- src/Mvc/Controller/ActionControllerInterface.php
+++ src/Mvc/Controller/ActionControllerInterface.php
@@ -5,11 +5,14 @@
  */
 namespace MSBios\CPanel\Mvc\Controller;
 
+use MSBios\Guard\GuardInterface;
+use Zend\Permissions\Acl\Resource\ResourceInterface;
+
 /**
  * Interface ActionControllerInterface
  * @package MSBios\CPanel\Mvc\Controller
  */
-interface ActionControllerInterface
+interface ActionControllerInterface extends GuardInterface, ResourceInterface
 {
-
+    // ...
 }
