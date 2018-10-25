9cfe1b88ca62be8fd1ca42bb747700eb540cf691
Judzhin Miles <info@msbios.com>
2018-10-25 10:48:39 +0300

    Intermedaite commit

--- src/Module.php
+++ src/Module.php
@@ -26,7 +26,7 @@ class Module implements
     AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.42';
+    const VERSION = '1.0.43';
 
     /**
      * @return mixed
